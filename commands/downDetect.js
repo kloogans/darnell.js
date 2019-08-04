0b7756014bbac790afb5c3e5ebd072d79d41d7c4
notjamesobrien <jtwebco@gmail.com>
2019-08-04 18:07:48 -0400

    fixes down detector

--- commands/downDetect.js
+++ commands/downDetect.js
@@ -1,22 +1,71 @@
 const fetch = require('node-fetch')
 
-exports.downDetect = async (richEmbed, msg, command) => {
-  const url = command.trim().split(' ').pop(),
-        res = await fetch(`https://isitdown.site/api/v3/${url}`),
-        json = await res.json(),
-        isDown = json.isitdown
+const request = require('request')
 
-  let message
+exports.downDetect = (richEmbed, msg, command) => {
+  const url = command.trim().split(' ').pop()
 
-  if (!isDown) {
-    message = richEmbed.setDescription(`**${url}** is up`)
-                       .setThumbnail('https://imgur.com/fATQP1I.png')
-                       .setColor('#3BCC58')
-    msg.channel.send(message)
+  if (isValidURL(url)) {
+    let message
+    request('https://' + url, (error, res, body) => {
+
+      if (error) console.log(error)
+      if (res) {
+        if (res.statusCode === 200 || res.statusCode === 201 || res.statusCode === 202) {
+          message = richEmbed.setTitle(res.statusCode)
+                             .setDescription(`**${url}** is up`)
+                             .setThumbnail('https://imgur.com/fATQP1I.png')
+                             .setColor('#3BCC58')
+          msg.channel.send(message)
+        } else if (res.statusCode === 300 || res.statusCode === 301) {
+          message = richEmbed.setTitle(res.statusCode)
+                             .setDescription(`**${url}** is redirecting`)
+                             .setThumbnail('https://imgur.com/vX3Z7i0.png')
+                             .setColor('#fffc00')
+          msg.channel.send(message)
+        } else {
+          message = richEmbed.setTitle(res.statusCode)
+                             .setDescription(`**${url}** is down`)
+                             .setThumbnail('https://imgur.com/vX3Z7i0.png')
+                             .setColor('#FF2C00')
+          msg.channel.send(message)
+        }
+      } else {
+        msg.channel.send('Either the URL doesn\'t exist or something went wrong.')
+      }
+    })
+
   } else {
-    message = richEmbed.setDescription(`**${url}** is down`)
-                       .setThumbnail('https://imgur.com/vX3Z7i0.png')
-                       .setColor('#FF2C00')
-    msg.channel.send(message)
+    msg.channel.send('not a valid URL')
   }
+
+
+
+
+
+
+
+
+
+
+
+
+  //
+  // if (!isDown) {
+  //   message = richEmbed.setDescription(`**${url}** is up`)
+  //                      .setThumbnail('https://imgur.com/fATQP1I.png')
+  //                      .setColor('#3BCC58')
+  //   msg.channel.send(message)
+  // } else {
+  //   message = richEmbed.setDescription(`**${url}** is down`)
+  //                      .setThumbnail('https://imgur.com/vX3Z7i0.png')
+  //                      .setColor('#FF2C00')
+  //   msg.channel.send(message)
+  // }
+}
+
+
+const isValidURL = url => {
+  let input = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
+  return (input !== null)
 }
