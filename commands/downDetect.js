f7f9c948cc1937d1bbdee2863531e097bec110bf
notjamesobrien <jtwebco@gmail.com>
2019-08-04 11:59:07 -0400

    structure change in stastus message response

--- commands/downDetect.js
+++ commands/downDetect.js
@@ -9,12 +9,12 @@ exports.downDetect = async (richEmbed, msg, command) => {
   let message
 
   if (!isDown) {
-    message = richEmbed.setTitle(`${url} is up`)
+    message = richEmbed.setDescription(`**${url}** is up`)
                        .setThumbnail('https://imgur.com/fATQP1I.png')
                        .setColor('#3BCC58')
     msg.channel.send(message)
   } else {
-    message = richEmbed.setTitle(`${url} is down`)
+    message = richEmbed.setDescription(`**${url}** is down`)
                        .setThumbnail('https://imgur.com/vX3Z7i0.png')
                        .setColor('#FF2C00')
     msg.channel.send(message)
