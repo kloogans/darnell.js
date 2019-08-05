13f4176b05bf26d6185a309e3b1dac7f59de2f67
notjamesobrien <jtwebco@gmail.com>
2019-08-05 10:30:43 -0400

    remove old comments

--- commands/downDetect.js
+++ commands/downDetect.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch')
-
 const request = require('request')
 
 exports.downDetect = (richEmbed, msg, command) => {
@@ -38,32 +36,7 @@ exports.downDetect = (richEmbed, msg, command) => {
   } else {
     msg.channel.send('not a valid URL')
   }
-
-
-
-
-
-
-
-
-
-
-
-
-  //
-  // if (!isDown) {
-  //   message = richEmbed.setDescription(`**${url}** is up`)
-  //                      .setThumbnail('https://imgur.com/fATQP1I.png')
-  //                      .setColor('#3BCC58')
-  //   msg.channel.send(message)
-  // } else {
-  //   message = richEmbed.setDescription(`**${url}** is down`)
-  //                      .setThumbnail('https://imgur.com/vX3Z7i0.png')
-  //                      .setColor('#FF2C00')
-  //   msg.channel.send(message)
-  // }
 }
-
 
 const isValidURL = url => {
   let input = url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
